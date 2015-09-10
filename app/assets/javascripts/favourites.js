7cfa0d96672c81732809338f134144d40a6d4933
schlorantics <schuolee@gmail.com>
2015-09-10 19:00:07 +0000

    Updated favourites feature to have classes reflected upon app refresh

--- app/assets/javascripts/favourites.js
+++ app/assets/javascripts/favourites.js
@@ -12,34 +12,33 @@ $(function(){
     }
     console.log('favourite clicked')
   });
-   
-    function favourite(user_id, listing_id, that){ 
-      // console.log(that)
-      $.ajax({
-        url: '/users/' + user_id + '/favourites',
-        type: "POST",
-        data: {user_id: user_id, listing_id: listing_id },
-        success: function(){ 
-          that.addClass('favourited')
-          that.removeClass('favourite_btn')
-          console.log('favourited')
-        }
-      });
-    }
+ 
+  function favourite(user_id, listing_id, that){ 
+    $.ajax({
+      url: '/users/' + user_id + '/favourites',
+      type: "POST",
+      data: {user_id: user_id, listing_id: listing_id },
+      success: function(){ 
+        that.addClass('favourited')
+        that.removeClass('unfavourite')
+        console.log('favourited')
+      }
+    });
+  }
     
-    function unfavourite(user_id, listing_id, that){
-    
-      $.ajax({
-        url: '/users/' + user_id + '/favourites?listing_id=' + listing_id,
-        type: "DELETE",
-        success: function(){
-          that.addClass('favourite_btn');
-          that.removeClass('favourited');
-            console.log('unfavourited')
-        }
-      });    
+  function unfavourite(user_id, listing_id, that){
+  
+    $.ajax({
+      url: '/users/' + user_id + '/favourites?listing_id=' + listing_id,
+      type: "DELETE",
+      success: function(){
+        that.addClass('unfavourite');
+        that.removeClass('favourited');
+          console.log('unfavourited')
+      }
+    });    
 
-    }
+  }
 
 });
 
