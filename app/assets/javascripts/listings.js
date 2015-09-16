85b58ca770b0c413ba44674833bd1ddeb0c4e921
schlorantics <schuolee@gmail.com>
2015-09-16 20:04:31 +0000

    Ryan (@KludgeMaster) fixed changes to ensure maps render across the website

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -14,7 +14,7 @@ function initMap() {
           zoom: 14,
         });
     getAddresses();
-    if (document.location.pathname.match(/(\d)+$/)) {
+    if (document.location.pathname.match(/listings\/(\d)+$/)) {
       importFoursquare();
       // getWalkScore(); // walkscore requires API key which is not available at this
     }
@@ -166,50 +166,4 @@ $(function(){
     geocodeAddress(geocoder, address);
   })
 
-  $('#filter_form').on('submit', function(event){
-    event.preventDefault;
-    var city = $('#city').val();
-    var input = $(this).serialize();
-
-    $.ajax({
-      url: '/listings/results?' + input,
-      dataType: "json",
-      success: function(data, status){
-        deleteMarkers();
-        var geocoder = new google.maps.Geocoder();
-        for (var i =0; i < data.length; i++){
-          geocodeAddress(geocoder, data[i].address)
-        }
-        $('#listing_results').html("");
-        if (data){
-          data.forEach(function(result){
-            var tr = $("<tr>");
-            var td = $("<td>").text(result.user_id).appendTo(tr);
-            var td = $("<td>").text(result.square_footage).appendTo(tr);
-            var td = $("<td>").text(result.bedroom).appendTo(tr);
-            var td = $("<td>").text(result.bathroom).appendTo(tr);
-            var td = $("<td>").text(result.price).appendTo(tr);
-            var td = $("<td>").text(result.address).appendTo(tr);
-            var td = $("<td>").text(result.furnished).appendTo(tr);
-            var td = $("<td>").text(result.pets).appendTo(tr);
-            var td = $("<td>").text(result.smoking).appendTo(tr);
-            var td = $("<td>").text(result.floor_number).appendTo(tr);
-            var td = $("<td>").text(result.parking_space).appendTo(tr);
-            var td = $("<td>").text(result.storage_space).appendTo(tr);
-            var td = $("<td>").text(result.balcony).appendTo(tr);
-            var td = $("<td>").text(result.available_date).appendTo(tr);
-            var td = $("<td>").text(result.minimum_lease).appendTo(tr);
-            var td = $("<td>").text(result.image).appendTo(tr);
-            var td = $("<td>").text(result.title).appendTo(tr);
-            var td = $("<td>").text(result.rental_type).appendTo(tr);
-            var td = $("<td>").text(result.description).appendTo(tr);
-            tr.appendTo('#listing_results')
-
-          });
-        }
-      }
-    })
-
-    return false;
-  });
 });
